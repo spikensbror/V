7e93250ec351d36fcb7aa3636e2709a0a9760821
Kimmy Andersson <cyaspik@gmail.com>
2013-01-16 10:58:33 +0100

    Added status accessor to HTTP module.

--- V/HTTP/Module.php
+++ V/HTTP/Module.php
@@ -5,11 +5,19 @@ namespace V\HTTP;
 class Module extends \V\Core\Module
 {
 	private $_router = null;
+	private $_status = null;
 
 	public function route()
 	{
 		return ($this->_router)
 			? $this->_router
 			: ($this->_router = new Router());
+	}
+
+	public function status()
+	{
+		return ($this->_status)
+			? $this->_status
+			: ($this->_status = new Status());
 	}
 }
